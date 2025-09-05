458e8fe090e55e3c9cd0f12e7e2ec57ee46f546f
nachobazan2006 <nacho68bazan@gmail.com>
2025-09-05 13:14:34 -0300

    Funcion finalizada con contador

--- pryBazanContacto/Form1.cs
+++ pryBazanContacto/Form1.cs
@@ -8,11 +8,14 @@ namespace pryBazanContacto
         }
         string vContacto = "";
         string vTelefono = "";
+        int    vContador = 0;
         private void btnGrabar_Click(object sender, EventArgs e)
         {
+            vContador++;
             vContacto = txtContacto.Text;
             vTelefono = txtTelefono.Text;   
-            lstResultado.Items.Add(vContacto + " - " + vTelefono);
+            string vResultado= vContador + " - " + vContacto + " - " + vTelefono;
+            lstResultado.Items.Add(vResultado);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
